7c01f8b3a63e65dfd8bff6da4c424fff43472003
oranjan <rnjnmhta@gmail.com>
2025-12-31 14:25:26 +0530

    ex 1.7

--- ex.1.6-1.14/src/App.jsx
+++ ex.1.6-1.14/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const sum = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / sum;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -18,6 +21,9 @@ function App() {
         <li>good {good}</li>
         <li>neutral {neutral}</li>
         <li>bad {bad}</li>
+        <li>all {sum}</li>
+        <li>average {average}</li>
+        <li>positive {good/sum *100}%</li>
       </ul>
     </div>
   );
