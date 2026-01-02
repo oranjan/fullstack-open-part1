94d932d3eefc0987df88da7e5589b03f548fc861
oranjan <rnjnmhta@gmail.com>
2026-01-02 16:15:34 +0530

    repetition fix

--- courseinfo/src/components/Content.jsx
+++ courseinfo/src/components/Content.jsx
@@ -6,7 +6,7 @@ export default function Content(props) {
     <>
       <Part part={parts[0].name} exercise={parts[0].exercises} />
       <Part part={parts[1].name} exercise={parts[1].exercises} />
-      <Part part={parts[1].name} exercise={parts[1].exercises} />
+      <Part part={parts[2].name} exercise={parts[2].exercises} />
     </>
   );
 }
